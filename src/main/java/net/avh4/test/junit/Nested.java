bcb54a4a4540873134849de7113f94aabe4c7196
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-08-13 20:09:27 -0700

    Inline Nested.InnerClassesRunner

--- src/main/java/net/avh4/test/junit/Nested.java
+++ src/main/java/net/avh4/test/junit/Nested.java
@@ -4,17 +4,15 @@ import org.junit.runner.Description;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
-import org.junit.runners.ParentRunner;
 import org.junit.runners.model.InitializationError;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.ArrayList;
 
 public class Nested extends Runner {
     private final BlockJUnit4ClassRunner outerClassRunner;
-    private final ParentRunner<Class<?>> innerClassesRunner;
     private final Class<?> testClass;
+    private final ArrayList<InnerSpecMethodRunner> childRunners =
+            new ArrayList<>();
 
     public Nested(final Class<?> testClass) throws InitializationError {
         BlockJUnit4ClassRunner outerClassRunner;
@@ -24,8 +22,11 @@ public class Nested extends Runner {
             outerClassRunner = null;
         }
         this.outerClassRunner = outerClassRunner;
-        innerClassesRunner = new InnerClassesRunner(testClass);
         this.testClass = testClass;
+
+        for (Class<?> aClass : testClass.getDeclaredClasses()) {
+            childRunners.add(new InnerSpecMethodRunner(aClass));
+        }
     }
 
     @Override
@@ -36,9 +37,8 @@ public class Nested extends Runner {
         } else {
             suiteDescription = Description.createSuiteDescription(testClass);
         }
-        for (Description childDescription : innerClassesRunner.getDescription()
-                .getChildren()) {
-            suiteDescription.addChild(childDescription);
+        for (InnerSpecMethodRunner childRunner : childRunners) {
+            suiteDescription.addChild(childRunner.getDescription());
         }
         return suiteDescription;
     }
@@ -48,37 +48,8 @@ public class Nested extends Runner {
         if (outerClassRunner != null) {
             outerClassRunner.run(notifier);
         }
-        innerClassesRunner.run(notifier);
-    }
-
-    private static class InnerClassesRunner extends ParentRunner<Class<?>> {
-        private final HashMap<Class<?>, InnerSpecMethodRunner> runners =
-                new HashMap<>();
-        private final List<Class<?>> classChildren;
-
-        public InnerClassesRunner(Class<?> testClass)
-                throws InitializationError {
-            super(testClass);
-
-            classChildren = Arrays.asList(testClass.getDeclaredClasses());
-            for (Class<?> aClass : classChildren) {
-                runners.put(aClass, new InnerSpecMethodRunner(aClass));
-            }
-        }
-
-        @Override
-        protected List<Class<?>> getChildren() {
-            return classChildren;
-        }
-
-        @Override
-        protected Description describeChild(Class<?> child) {
-            return runners.get(child).getDescription();
-        }
-
-        @Override
-        protected void runChild(Class<?> child, RunNotifier notifier) {
-            runners.get(child).run(notifier);
+        for (InnerSpecMethodRunner childRunner : childRunners) {
+            childRunner.run(notifier);
         }
     }
 }
